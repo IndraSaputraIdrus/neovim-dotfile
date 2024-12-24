210fe7fe7274d2d4f763076c8451a4185dca1225
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-12-24 17:20:21 +0000

    chore: setup formatter

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,25 @@
+return {
+	"stevearc/conform.nvim",
+	dependencies = { "mason.nvim" },
+	lazy = true,
+	cmd = "ConformInfo",
+	keys = {
+		{
+			"<leader>fm",
+			function()
+				require("conform").format()
+				print("formatting")
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			svelte = { "prettierd" },
+			typescript = { "prettierd" },
+			javascript = { "prettierd" },
+		},
+	},
+}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,30 +1,30 @@
 return {
-  {
-    "echasnovski/mini.bufremove",
-    version = '*', -- Gunakan versi stable
-    lazy = true,   -- Aktifkan lazy loading
-    keys = {
-      {
-        "<Leader>bd",
-        function()
-          require("mini.bufremove").delete(0, false)
-        end,
-        desc = "Hapus buffer tanpa memaksa"
-      },
-      {
-        "<Leader>bD",
-        function()
-          require("mini.bufremove").delete(0, true)
-        end,
-        desc = "Hapus buffer dengan paksa"
-      },
-      {
-        "<Leader>x",
-        function()
-          require("mini.bufremove").wipeout(0, false)
-        end,
-        desc = "Wipeout buffer"
-      },
-    },
-  },
+	{
+		"echasnovski/mini.bufremove",
+		version = "*", -- Gunakan versi stable
+		lazy = true, -- Aktifkan lazy loading
+		keys = {
+			{
+				"<Leader>bd",
+				function()
+					require("mini.bufremove").delete(0, false)
+				end,
+				desc = "Hapus buffer tanpa memaksa",
+			},
+			{
+				"<Leader>bD",
+				function()
+					require("mini.bufremove").delete(0, true)
+				end,
+				desc = "Hapus buffer dengan paksa",
+			},
+			{
+				"<Leader>x",
+				function()
+					require("mini.bufremove").wipeout(0, false)
+				end,
+				desc = "Wipeout buffer",
+			},
+		},
+	},
 }
