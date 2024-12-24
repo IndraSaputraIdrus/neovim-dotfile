aa9b68ab955c91d92dafe2145cb7f9e4f923634f
indra saputra idrus <indrasaputraidrus@gmail.com>
2024-12-24 14:27:03 +0000

    init

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "echasnovski/mini.bufremove",
+    version = '*', -- Gunakan versi stable
+    lazy = true,   -- Aktifkan lazy loading
+    keys = {
+      {
+        "<Leader>bd",
+        function()
+          require("mini.bufremove").delete(0, false)
+        end,
+        desc = "Hapus buffer tanpa memaksa"
+      },
+      {
+        "<Leader>bD",
+        function()
+          require("mini.bufremove").delete(0, true)
+        end,
+        desc = "Hapus buffer dengan paksa"
+      },
+      {
+        "<Leader>x",
+        function()
+          require("mini.bufremove").wipeout(0, false)
+        end,
+        desc = "Wipeout buffer"
+      },
+    },
+  },
+
+  {
+    'echasnovski/mini.trailspace',
+    version = '*',
+    opts = {}, init = function()
+      vim.api.nvim_create_user_command('Trim', function()
+          MiniTrailspace.trim()
+      end, {})
+    end
+  },
+}
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,9 @@
+return { 
+  'olivercederborg/poimandres.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  init = function()
+    vim.cmd('colorscheme poimandres')
+  end
+}
