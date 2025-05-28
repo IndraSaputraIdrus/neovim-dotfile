9ef839b9ba2d7fb782d7180d5068d725c7c42764
nix-drz <indrasaputraidrus@gmail.com>
2025-05-28 23:01:23 +0800

    telescope & which-key

--- lua/ndrainz/plugins/linter.lua
+++ lua/ndrainz/plugins/linter.lua
@@ -12,17 +12,13 @@ return {
       svelte = { 'eslint_d' },
     }
 
-    -- Create autocommand which carries out the actual linting
-    -- on the specified events.
-    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    local linter_autogroup = vim.api.nvim_create_augroup('lint', { clear = true })
+
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-      group = lint_augroup,
+      group = linter_autogroup,
       callback = function()
-        -- Only run the linter in buffers that you can modify in order to
-        -- avoid superfluous noise, notably within the handy LSP pop-ups that
-        -- describe the hovered symbol using Markdown.
         if vim.bo.modifiable then
-          lint.try_lint()
+          linter.try_lint()
         end
       end,
     })
