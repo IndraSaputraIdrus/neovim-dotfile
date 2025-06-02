ddee7bcd4a440f68d50c5fd1f4aa1d6d740bb13c
nix-drz <indrasaputraidrus@gmail.com>
2025-06-02 21:18:38 +0800

    update config blink.cmp

--- lua/ndrainz/plugins/completion.lua
+++ lua/ndrainz/plugins/completion.lua
@@ -1,6 +1,5 @@
 return {
   'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
   event = { 'VimEnter' },
   version = '1.*',
   dependencies = {
@@ -8,84 +7,87 @@ return {
     {
       'L3MON4D3/LuaSnip',
       version = '2.*',
-      build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
       opts = {},
     },
     'folke/lazydev.nvim',
+    'rafamadriz/friendly-snippets',
   },
 
   --- @module 'blink.cmp'
   --- @type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
     keymap = { preset = 'enter' },
-
-    -- Don't show completion menu automatically in cmdline mode
     completion = {
-      -- By default, you may press `<c-space>` to show the documentation.
-      -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
-
+      documentation = {
+        auto_show = false,
+        auto_show_delay_ms = 500,
+        window = {
+          border = nil,
+          scrollbar = false,
+          winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
+        },
+      },
       menu = {
-        auto_show = function(ctx)
-          return ctx.mode ~= 'cmdline'
-        end,
+        border = nil,
+        scrollbar = false,
+        scrolloff = 1,
+        draw = {
+          columns = {
+            { 'kind_icon' },
+            { 'label', 'label_description', gap = 1 },
+            { 'kind' },
+            { 'source_name' },
+          },
+        },
       },
     },
 
     appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
       use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono',
     },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = {
+        'lsp',
+        'path',
+        'snippets',
+        'buffer',
+      },
       providers = {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+        cmdline = {
+          min_keyword_length = 2,
+        },
+        snippets = {
+          min_keyword_length = 2,
+        },
+        lsp = {
+          min_keyword_length = 0,
+          score_offset = 0,
+        },
+        path = {
+          min_keyword_length = 0,
+        },
+        buffer = {
+          min_keyword_length = 4,
+          max_items = 5,
+        },
       },
     },
 
     snippets = { preset = 'luasnip' },
-
-    -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-    -- which automatically downloads a prebuilt binary when enabled.
-    --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
-    -- See :h blink-cmp-config-fuzzy for more information
     fuzzy = { implementation = 'lua' },
-
-    -- Shows a signature help window while you type arguments for a function
     signature = { enable = true },
   },
+
+  config = function(_, opts)
+    vim.cmd 'highlight Pmenu guibg=none'
+    vim.cmd 'highlight PmenuExtra guibg=none'
+    vim.cmd 'highlight FloatBorder guibg=none'
+    vim.cmd 'highlight NormalFloat guibg=none'
+
+    require('blink.cmp').setup(opts)
+    require('luasnip.loaders.from_vscode').lazy_load()
+  end,
 }
